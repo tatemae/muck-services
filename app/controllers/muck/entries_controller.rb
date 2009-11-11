ac367ec949bf0ce0a9bd084845b25dca0cb40314
Joel Duffin <Joel@joel-duffins-macbook.local>
2009-11-10 20:08:08 -0700

    Fixed track_clicks bug

--- app/controllers/muck/entries_controller.rb
+++ app/controllers/muck/entries_controller.rb
@@ -63,8 +63,8 @@ class Muck::EntriesController < ApplicationController
   
   def track_clicks
     user_agent = request.env['HTTP_USER_AGENT']
-    redirect_to Entry.track_click(session, params[:id], request.env['HTTP_REFERER'], params[:target], request.env['HTTP_X_FORWARDED_FOR'], user_agent) if !/Googlebot/.match(user_agent)
-    else redirect_to '/'
+    redirect_to Entry.track_click(session, params[:id], request.env['HTTP_REFERER'], params[:target], request.env['HTTP_X_FORWARDED_FOR'], user_agent) if !/Googlebot/.match(user_agent) and return
+    else redirect_to '/' and return
   end
   
   def collections
