650de242b18462f2fabceea04e40c323b391dc47
Joel Duffin <Joel@Joel-Duffins-iMac.local>
2010-02-03 13:13:31 -0700

    Synced test app w/ muck gems

--- test/rails_root/db/schema.rb
+++ test/rails_root/db/schema.rb
@@ -9,7 +9,28 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091118203605) do
+ActiveRecord::Schema.define(:version => 20100123233654) do
+
+  create_table "access_code_requests", :force => true do |t|
+    t.string   "email"
+    t.datetime "code_sent_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "access_code_requests", ["email"], :name => "index_access_code_requests_on_email"
+
+  create_table "access_codes", :force => true do |t|
+    t.string   "code"
+    t.integer  "uses",       :default => 0,     :null => false
+    t.boolean  "unlimited",  :default => false, :null => false
+    t.datetime "expires_at"
+    t.integer  "use_limit",  :default => 1,     :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "access_codes", ["code"], :name => "index_access_codes_on_code"
 
   create_table "activities", :force => true do |t|
     t.integer  "item_id"
@@ -462,6 +483,7 @@ ActiveRecord::Schema.define(:version => 20091118203605) do
     t.datetime "updated_at"
     t.string   "identity_url"
     t.string   "url_key"
+    t.integer  "access_code_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email"
